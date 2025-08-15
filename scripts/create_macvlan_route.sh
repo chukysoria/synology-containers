1f422030a0b8e1ba8f9bfe3c31980c38729b5125
Carlos Sánchez <chukysoria@users.noreply.github.com>
2025-08-15 13:11:27 +0000

    ⬆️ Mejora la configuración de Jellyfin y agrega soporte para Tailscale en docker-compose

--- scripts/create_macvlan_route.sh
+++ scripts/create_macvlan_route.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-# Configure to execute after Synology reboot
-# get interface name (ovs_eth0 below) via ip link. Currenly ovs_nomd0 as Virtual Machine is installed and bond used
+## get interface name (ovs_eth0 below) via ip link. Currenly ovs_bond0 as Virtual Machine is installed and bond used
 ip link add macvlan0 link ovs_bond0 type macvlan mode bridge
-# Set address of the host to 192.168.1.48 within he macvlan network of ip range 192.168.1.48/28
-ip addr add 192.168.1.48/28 dev macvlan0
+# Set address of the host to 192.168.1.48/32
+ip addr add 192.168.1.48/32 dev macvlan0
 # Up internface
 ip link set macvlan0 up
+# Add the route for range 192.168.1.48/28
+ip route add 192.168.1.48/28 dev macvlan0
